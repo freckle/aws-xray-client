233b48f7c3e748af83b75a799b7b857bba8ba4c6
✿ corey (he/they) <corey@x64.co>
2022-02-25 11:19:37 -0800

    Wrap import in preprocessor directive

--- aws-xray-client-persistent/library/Network/AWS/XRayClient/Persistent.hs
+++ aws-xray-client-persistent/library/Network/AWS/XRayClient/Persistent.hs
@@ -18,8 +18,10 @@ import Database.Persist
 import Database.Persist.Sql
 import Database.Persist.Sql.Types.Internal
   (IsPersistBackend(mkPersistBackend), SqlBackend(..))
+#if MIN_VERSION_persistent(2,13,3)
 import Database.Persist.SqlBackend.StatementCache
   (StatementCache, mkSimpleStatementCache, mkStatementCache)
+#endif
 import Network.AWS.XRayClient.Segment
 import Network.AWS.XRayClient.TraceId
 import System.Random
